67ac13423fa94d3cf746943176a3b0717242b53d
André Baldeweg <a@baldeweg.org>
2022-10-12 17:58:24 +0000

    improvements

--- mission/create/create.go
+++ mission/create/create.go
@@ -9,44 +9,41 @@ import (
 )
 
 type Logfile struct {
-    Notes []string `json:"notes"`
-    Replacements map[string]string `json:"replacements"`
-    Missions []Mission `json:"missions"`
+	Notes        []string          `json:"notes"`
+	Replacements map[string]string `json:"replacements"`
+	Missions     []Mission         `json:"missions"`
 }
 
 type Mission struct {
-    Date string `json:"date"`
-    Time string `json:"time"`
-    Keyword string `json:"keyword"`
-    Situation string `json:"situation"`
-    Unit string `json:"unit"`
-    Location string `json:"location"`
-    Links []string `json:"links"`
-    Private bool `json:"private"`
+	Date      string   `json:"date"`
+	Time      string   `json:"time"`
+	Keyword   string   `json:"keyword"`
+	Situation string   `json:"situation"`
+	Unit      string   `json:"unit"`
+	Location  string   `json:"location"`
+	Links     []string `json:"links"`
+	Private   bool     `json:"private"`
 }
 
+func Create() {
+	filename := "mission.json"
 
+	create := Mission{
+		Date: time.Now().Format("2006-01-02"),
+		Time: time.Now().Format("15:04"),
+	}
 
-func Create(){
-    filename := "mission.json"
-
-    create := Mission{
-        Date: time.Now().Format("2006-01-02"),
-        Time: time.Now().Format("15:04"),
-    }
-
-    var t Logfile
+	var t Logfile
 	if err := json.Unmarshal([]byte(string(storage.Read(filename))), &t); err != nil {
 		log.Fatal(err)
 	}
 
-    t.Missions = append(t.Missions, create)
+	t.Missions = append(t.Missions, create)
 
+	d, err := json.Marshal(&t)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    d, err := json.Marshal(&t)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    storage.Write(filename, string(d))
+	storage.Write(filename, string(d))
 }
